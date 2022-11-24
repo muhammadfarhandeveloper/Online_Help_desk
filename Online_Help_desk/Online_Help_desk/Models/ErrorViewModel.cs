13191e7d274254cdee741b236ce44e618ca2c4e0
= <mohammadfarhan44500@gmail.com>
2022-11-24 20:29:25 +0500

    first time addes files

---
+++ Online_Help_desk/Online_Help_desk/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Online_Help_desk.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
